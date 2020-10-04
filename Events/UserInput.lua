6fb4108fd1cd4d5c46dd0d9b19c76621a91efa55
Jcook894 <jcook894@icloud.com>
2020-10-03 23:12:23 -0400

    Events: Player will now move based on user input.

--- Events/UserInput.lua
+++ Events/UserInput.lua
@@ -1,19 +1,18 @@
 require 'Player.player'
-local up, down, left, right = love.keyboard.isDown("w"), love.keyboard.isDown("s"), love.keyboard.isDown("a"), love.keyboard.isDown("d")
 
 -- Get keyboard input and update player based on key pressed.
 function Move_Player()
-    if up then 
-        Player.x = Player.x + 1
-        print(Player.x)
-    elseif down then
-        Player.x = Player.x - 1
-        print(Player.x)
-    elseif left then
+    if love.keyboard.isDown('w') then
         Player.y = Player.y - 1
         print(Player.y)
-    elseif right then
+    elseif love.keyboard.isDown('s') then
         Player.y = Player.y + 1
+        print(Player.x)
+    elseif love.keyboard.isDown('a') then
+        Player.x = Player.x - 1
+        print(Player.y)
+    elseif love.keyboard.isDown('d') then
+        Player.x = Player.x + 1
         print(Player.y)
     end
 end
