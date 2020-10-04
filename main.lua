ef6c2a32729fa3eede0534824ee22a1fd9e636ea
Jcook894 <jcook894@icloud.com>
2020-10-03 23:21:55 -0400

    Added title to game window.

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
+require 'Assets'
 require 'Player.player'
 require 'Events.UserInput'
 
 function love.load()
     Player.name = 'Julian Cook';
+    love.window.setTitle("Julian's Prototype")
 end
 
 function love.update(dt)
